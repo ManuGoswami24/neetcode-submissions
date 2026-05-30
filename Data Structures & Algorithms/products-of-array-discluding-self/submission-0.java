8e50e1200af897286b34204f997a6cea137a3994
ManuGoswami24 <93631236+ManuGoswami24@users.noreply.github.com>
2026-05-31 01:12:28 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int size = nums.length;
+        int [] prefix = new int[size];
+        int [] suffix = new int[size];
+        int [] res = new int[size];
+
+        prefix[0]=1;
+        suffix[size-1]=1;
+
+        for (int i=1; i<size; i++){
+            prefix[i] = nums[i-1]*prefix[i-1];
+        }
+        for (int i=size-2; i>=0 ; i--){
+            suffix[i] = nums[i+1]*suffix[i+1];
+        }
+        for (int i=0; i<size; i++){
+            res[i]=prefix[i]*suffix[i];
+        }
+        return res;
+        
+    }
+}
---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i=0 ; i<nums.length ;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> map.get(a) - map.get(b));
+
+        for (int i: map.keySet()){
+            pq.add(i);
+
+            if (pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        int i=0;
+        for (int n:pq){
+            result[i++]=n;
+        }
+
+        return result;
+        
+    }
+}
